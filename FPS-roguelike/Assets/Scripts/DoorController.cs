fef0efcef4cfd1770d4bfe45b9c3eabd29d16925
Ruslan <1ghawk1@mail.ru>
2024-11-24 18:37:01 +0300

    added door

---
+++ FPS-roguelike/Assets/Scripts/DoorController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public float rotationSpeed = 2.0f; // Скорость вращения
+    public float viewAngle = 30.0f; 
+    private bool isOpen = false; // Состояние двери
+    private bool playerInside = false; // Игрок внутри двери
+    private Quaternion closedRotation; // Закрытое положение
+    private Quaternion openRotation; // Открытое положение
+
+    private AudioSource audioSource; // Компонент AudioSource
+    public AudioClip openSound; // Звук открытия двери
+    public AudioClip closeSound; // Звук закрытия двери
+
+    public AudioClip closedSound; // Звук запертой двери
+    private void Start()
+    {
+        closedRotation = transform.rotation; // Сохраняем начальное положение двери
+        openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, -90, 0)); // Определяем открытое положение
+        audioSource = GetComponent<AudioSource>(); // Получаем компонент AudioSource
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) // Проверяем нажатие клавиши E и что игрок не внутри двери
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            if (IsPlayerLookingAtDoor(player) && distanceToPlayer <= 3.0f ){
+                if (!playerInside) // Проверяем расстояние и состояние двери
+                {
+                    ToggleDoor();
+                }
+                else
+                {
+                    audioSource.PlayOneShot(closedSound); // Звук запертой двери
+                }
+            }
+        }
+
+        // Плавно поворачиваем дверь
+        if (isOpen)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, openRotation, Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, closedRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private bool IsPlayerLookingAtDoor(GameObject player)
+    {
+        // Получаем направление взгляда игрока
+        Vector3 playerDirection = player.transform.forward;
+        Vector3 directionToDoor = (transform.position - player.transform.position).normalized;
+
+        // Рассчитываем угол между направлениями
+        float angle = Vector3.Angle(playerDirection, directionToDoor);
+
+        // Проверяем, что угол меньше указанного
+        return angle < viewAngle;
+    }
+
+
+
+    private void ToggleDoor()
+    {
+        isOpen = !isOpen; // Меняем состояние двери
+
+        if (isOpen)
+        {
+            audioSource.PlayOneShot(openSound); // Воспроизводим звук открытия двери
+        }
+        else
+        {
+            audioSource.PlayOneShot(closeSound); // Воспроизводим звук закрытия двери
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isOpen)
+        {
+            playerInside = true; // Игрок зашёл за дверь
+            isOpen = false; // Закрыть дверь при входе
+            audioSource.PlayOneShot(closeSound);
+        }
+    }
+}
